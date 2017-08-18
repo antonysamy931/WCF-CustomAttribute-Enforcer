95176569fcaf99099f5c9c3a7b2b5307b88ef739
unknown <Antony_S@ravsoftw0252.MEDUSA.LOCAL>
2017-08-18 20:59:56 +0530

    Research wcf customer attribute

---
+++ Enforcer.Service/TestEnforcer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Web;
+
+namespace Enforcer.Service
+{
+    public abstract class EnforcerBehavior : Attribute, IParameterInspector, IOperationBehavior
+    {
+        public virtual object BeforeCall(string operationName, object[] inputs)
+        {
+            try
+            {
+                if (this.Validate(operationName, inputs))
+                {
+                    var obj = inputs[0] as IData;
+                    var obj1 = inputs[0] as ISample;
+                    return null;
+                }
+            }
+            catch
+            {
+                throw new FaultException("Access denied");
+            }            
+            throw new FaultException("Access denied");
+        }
+
+        public virtual void AfterCall(string operationName, object[] outputs, object returnValue, object correlationState)
+        {
+        }
+
+        public virtual void Validate(OperationDescription operationDescription)
+        {
+        }
+
+        public virtual void ApplyDispatchBehavior(OperationDescription operationDescription, DispatchOperation dispatchOperation)
+        {
+            dispatchOperation.ParameterInspectors.Add(this);
+        }
+
+        public virtual void ApplyClientBehavior(OperationDescription operationDescription, ClientOperation clientOperation)
+        {
+        }
+
+        public virtual void AddBindingParameters(OperationDescription operationDescription, BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public abstract bool Validate(string operationName, object[] inputs);
+    }
+
+    public class TestEnforcer : EnforcerBehavior
+    {
+        public override bool Validate(string operationName, object[] inputs)
+        {
+            return true;
+        }
+    }
+}
---
+++ Enforcer.Web/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Enforcer.Service;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Enforcer.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Service1Client client = new Service1Client();
+            var result = client.GetData(20);
+
+            //CompositeType type = new CompositeType();
+            //type.StringValue = "value";
+            //type.BoolValue = true;
+
+            //var com = client.GetDataUsingDataContract(type);
+
+            SampleRequest request = new SampleRequest();
+            request.Age = "10";
+            request.Name = "Antony";
+            client.TestInterfaceClass(request);
+            //string va = ContentTypeHelper.GetContentType(@"D:\Antony\sample.log");        
+            return View();
+        }
+
+    }
+
+    public static class ContentTypeHelper
+    {
+        /// <summary>
+        /// Gets the MIME type corresponding to the extension of the specified file name.
+        /// </summary>
+        /// <param name="fileName">The file name to determine the MIME type for.</param>
+        /// <returns>The MIME type corresponding to the extension of the specified file name, if found; otherwise, null.</returns>
+        public static string GetContentType(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+
+            if (String.IsNullOrWhiteSpace(extension))
+            {
+                return null;
+            }
+
+            var registryKey = Registry.ClassesRoot.OpenSubKey(extension);
+
+            if (registryKey == null)
+            {
+                return null;
+            }
+
+            var value = registryKey.GetValue("Content Type") as string;
+
+            return String.IsNullOrWhiteSpace(value) ? null : value;
+        }
+    }
+}
